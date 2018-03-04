0d8d43c15ec835ea635a7615fa10369a15c35405
Stephane M <s.manguette@gmail.com>
2018-03-04 21:19:52 +0100

    bug
    was'nt working since using structs instead of  unions

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,21 @@ void float2int(float *farg, int *intresult) { // Convert float 2 a signed int fo
   *intresult = *farg * 100;
 }
 
-struct intarray { // shared memory for int to get its bytes
+void bytes2int(float *farg, int *intresult) { // Convert float 2 a signed int for 2 decimal precision
+
+}
+
+union intarray { // shared memory for int and byte array to get its bytes
   int ints;
-  uint8_t bytes[2];
+  byte part[2];
 };
 
 /* to delete when setting up sensors */
 
 float tempext = 3.42;
 float tempeau = 17.24;
-intarray itempext;
-intarray itempeau;
+intarray itempext, itempeau;
+
 /* */
 
 void setup() {
@@ -26,21 +30,21 @@ void setup() {
     Serial.begin(9600);
     Serial.println("Initialising debug mode...");
 
-    float2int(&tempext, &itempext.ints); // converting floats to int and storing in structs objects
+    float2int(&tempext, &itempext.ints); // converting floats to int and storing in union objects
     float2int(&tempeau, &itempeau.ints);
 
     Serial.print("Outside temperature (°C) : ");
     Serial.println(float(itempext.ints)/100);
-    Serial.print('In bytes : ');
-    Serial.print(itempext.bytes[0],HEX);
+    Serial.print("In bytes : ");
+    Serial.print(itempext.part[0]);
     Serial.print(" ");
-    Serial.println(itempext.bytes[1],HEX);
+    Serial.println(itempext.part[1]);
     Serial.print("Water temperature (°C) : ");
     Serial.println(float(itempeau.ints)/100);
-    Serial.print('In bytes : ');
-    Serial.print(itempeau.bytes[0],HEX);
+    Serial.print("In bytes : ");
+    Serial.print(itempeau.part[0],HEX);
     Serial.print(" ");
-    Serial.println(itempeau.bytes[1],HEX);
+    Serial.println(itempeau.part[1],HEX);
   }
 
 }
