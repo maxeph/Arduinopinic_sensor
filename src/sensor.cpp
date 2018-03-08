dd08b44d10d936c1f6a10477c4173bb203f69dfc
Stephane M <s.manguette@gmail.com>
2018-03-08 23:36:26 +0100

    Implementing DHT

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -1,11 +1,14 @@
 #include <Arduino.h>
 #include <Manchester.h>  //Initialising 433 wireless library
-#include <FastCRC.h>  //CRCcheck library
+#include <FastCRC.h>  // CRCcheck library
+#include <DHT.h> // DHT library
 
 // Declaring definitions
 
 #define DEBUG 1 // if 1, debug with Serial
 #define TX_433 2 // Pin connecter to Transmitter
+#define DHTPIN 3 // Pin DHT
+#define DHTTYPE DHT22 // Pin DHT
 #define NBPARAM 3 // Number of int sent
 #define MSGLEN NBPARAM*2 // Msg len is 4 = 2 signed int (2 bytes each)
 #define PCKTLEN MSGLEN+3 // +1 for the lenght of the msgpacket +2 for CRC 16
@@ -22,6 +25,9 @@ union intarray { // shared memory for int and byte array to get its bytes
 byte msgpacket[PCKTLEN] = {PCKTLEN}; // init unsigned bytes to be sent over
 FastCRC16 CRC16; // init CRC16 object
 intarray itempext, itempeau, ihumid, crc_local;
+float tempext, tempeau = 17.24, humid; // floats to be used with sensors
+DHT dht(DHTPIN, DHTTYPE); // Declaring sensor
+int nloop = 1; // Counting n° Sending
 
 // Declaring functions
 
@@ -48,17 +54,10 @@ void buildpacket(byte msg[PCKTLEN], byte part1[2], byte part2[2], byte part3[2])
   msg[PCKTLEN-1] = 0;
 }
 
-/* to delete when setting up sensors in loop */
-
-float tempext = 3.42;
-float tempeau = 17.24;
-float humid = 48.24;
-
-/* */
-
 void setup() {
 
   man.setupTransmit(TX_433, MAN_600); // Initialising 433 wireless
+  dht.begin(); // Init DHT sensor
 
   if (DEBUG) {  // Sending over Serial to make sure it works
     Serial.begin(9600);
@@ -67,11 +66,29 @@ void setup() {
 
   // Here to move in loop when sensors in place
 
+  float2int(&tempeau, &itempeau.ints); // converting floats to int and storing in union objects
+
+}
+
+void loop() {
+  delay(5000);
+  tempext = dht.readTemperature();
+  humid = dht.readHumidity();
+  if (isnan(tempext) || isnan(humid)) {
+    if (DEBUG) {
+      Serial.println("Failed to read from DHT sensor!");
+      dht.begin();
+    }
+    return;
+  }
+
   float2int(&tempext, &itempext.ints); // converting floats to int and storing in union objects
-  float2int(&tempeau, &itempeau.ints);
   float2int(&humid, &ihumid.ints);
 
   if (DEBUG) { // Showing value received from sensors
+    Serial.print("########## PACKET N° "); // Showing raw data
+    Serial.print(nloop);
+    Serial.println(" #################");
     Serial.print("Outside temperature (°C) : ");
     Serial.println(float(itempext.ints)/100);
     Serial.print("In bytes : ");
@@ -95,7 +112,7 @@ void setup() {
   buildpacket(msgpacket,itempext.part,itempeau.part,ihumid.part); // Building packet to be sent over
 
   if (DEBUG) { // Showing raw data sent over
-    Serial.println("#######################################");
+    Serial.println("----------------------------------------");
     Serial.println("Raw data : ");
     Serial.print("Packet length : ");
     Serial.println(msgpacket[0],HEX);
@@ -124,12 +141,9 @@ void setup() {
     Serial.print(crc_local.part[0],HEX);
     Serial.print(" ");
     Serial.println(crc_local.part[1],HEX);
-    Serial.println("#######################################");
   }
-}
 
-void loop() {
   man.transmitArray(PCKTLEN, msgpacket);
   delay(5000);
-
+nloop++;
 }
